c5951682e27a8ece543045542c75c82efde25e68
RajendranEshwaran <rajendranmca2010@gmail.com>
2020-10-08 11:48:44 -0400

    Initial Commit

---
+++ FuritsListSwiftUI/FuritsListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuritsListSwiftUIApp.swift
+//  FuritsListSwiftUI
+//
+//  Created by gomathi saminathan on 10/8/20.
+//
+
+import SwiftUI
+
+@main
+struct FuritsListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
